77f1c1fc63d37702ad043e13fe92b94140309466
ooooo5h <ooooo5h91@gmail.com>
2022-01-03 16:38:25 +0900

    TextView의 UI를 코틀린에서 변경해보기   - text속성에 들어갈 값 새로 대입

--- app/src/main/java/com/neppplus/kotlinbasic_20220103/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20220103/MainActivity.kt
@@ -41,6 +41,16 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnMessageTextView.setOnClickListener {
+
+//            1. 입력된 문구를 받아서 저장해야함(input같은 동작이 필요해
+            val inputMessage = edtMessage.text.toString()
+
+//            2. txtMessage의 문구로 inputMessage 반영
+            txtMessage.text = "${inputMessage} 입력됨"
+
+        }
+
 
     }
 }
